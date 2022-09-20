ee9c62b0b22259948a7db8ee7322022751174de7
nexbox09 <ramosjose.gt@gmail.com>
2022-09-20 09:35:28 -0600

    Código comentado

--- client.py
+++ client.py
@@ -2,33 +2,35 @@ import socket
 import sys
 import pickle
 
+## Definición de la clase Mensaje ##
 class Mensaje:
     emoji =""
     text = ""
+## Definición de la clase Mensaje ##
 
-s = socket.socket()
+s = socket.socket()                                                 #Crear socket
 host = input(str("Please enter the hostname of the server : "))
 port = 8080
-s.connect((host,port))
+s.connect((host,port))                                              #vincular
 name = input(str("Please enter your username : "))
 print(" Connected to chat server")
 
-s.send(name.encode())
-s_name = s.recv(1024)
+s.send(name.encode())                   
+s_name = s.recv(1024)                                               #Recibir el nombre de usario del servidor
 s_name = s_name.decode()
 print("")
 print(s_name, "has joined the chat room ")
 
 while 1:
-    mensaje = Mensaje()
+    mensaje = Mensaje()                                         #definir mensaje de clase Mensaje
     data = s.recv(4096)
-    data_variable = pickle.loads(data)
-    print(s_name, ":" ,data_variable.emoji,data_variable.text)
+    data_variable = pickle.loads(data)                          #Des-serializar el flujo de datos
+    print(s_name, ":" ,data_variable.emoji,data_variable.text)  #Imprimir los parámetros emoji y text de la variable data_variable
     print("")
     mensaje = Mensaje()
-    mensaje.emoji=  input(str("Please enter your emoji: "))
+    mensaje.emoji=  input(str("Please enter your emoji: "))     #recibir parámetros
     mensaje.text =  input(str("Please enter your text "))
-    message = pickle.dumps(mensaje)
+    message = pickle.dumps(mensaje)                             #Serializar jerarquía de objetos
     s.send(message)
     print("")
 
--- server.py
+++ server.py
@@ -3,40 +3,42 @@ import sys
 import pickle
 
 
-
+## Definición de la clase Mensaje ##
 class Mensaje:
     emoji =""
     text = ""
+## Definición de la clase Mensaje ##
 
-s = socket.socket()
-host = socket. gethostname()
-print(" server will start on host : ", socket.gethostbyname(socket.gethostname()))
+
+s = socket.socket()                                                                 #Crear socket
+host = socket. gethostname()                                                        #Método para obtener el hostname
+print(" server will start on host : ", socket.gethostbyname(socket.gethostname()))  #Mostrar dirección IP del servidor
 port = 8080
-s.bind((host,port))
-name = input(str("Please enter your username: "))
+s.bind((host,port))                                                                 #vincular
+name = input(str("Please enter your username: "))                           
 print("")
 print("Server is waiting for incoming connections")
 print("")
-s.listen(1)
-conn, addr = s.accept()
+s.listen(1)                                                                         #Escuchar las comunicaciones entrantes, 1 significa que solo 1 conexión es aceptada
+conn, addr = s.accept()                                                             #initiates a connection with the clien
 print("Recieved connection")
 print("")
-s_name = conn.recv(1024)
-s_name = s_name.decode()
-print(s_name, "has joined the chat room")
-conn.send(name.encode())
+s_name = conn.recv(1024)                                                            #Receive data from the socket. The return value is a bytes object representing the data received. 
+s_name = s_name.decode()                                                            #The maximum amount of data to be received at once is specified by bufsize
+print(s_name, "has joined the chat room")                                           #Recibir e imprimir nombre de usuario del cliente
+conn.send(name.encode())                                                            #Enviar nombre de usuario del servidor                                      
 
 while 1:
-    mensaje = Mensaje()
-    mensaje.emoji=  input(str("Please enter your emoji: "))
-    mensaje.text =  input(str("Please enter your text "))
-    message = pickle.dumps(mensaje)
-    conn.send(message)
+    mensaje = Mensaje()                                         #definir mensaje de clase Mensaje
+    mensaje.emoji=  input(str("Please enter your emoji: "))     #recibir parámetros
+    mensaje.text =  input(str("Please enter your text "))       
+    message = pickle.dumps(mensaje)                             #Serializar jerarquía de objetos
+    conn.send(message)                                          
     print("Sent")
     print("")
-    data = conn.recv(4096)
-    data_variable = pickle.loads(data)
-    print(s_name, ":" ,data_variable.emoji,data_variable.text)
+    data = conn.recv(4096)                                      #Recibir mensaje de cliente
+    data_variable = pickle.loads(data)                          #Des-serializar el flujo de datos
+    print(s_name, ":" ,data_variable.emoji,data_variable.text)  #Imprimir los parámetros emoji y text de la variable data_variable
     print("")
 
     
