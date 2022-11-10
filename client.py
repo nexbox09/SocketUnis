ce42c65402bfb1b9f77fd05a5c2421e43b50d31b
nexbox09 <ramosjose.gt@gmail.com>
2022-11-09 21:25:17 -0600

    Proyecto final

--- client.py
+++ client.py
@@ -1,36 +1,106 @@
 import socket
 import sys
 import pickle
+import time
+import random
+from datetime import datetime
+from typing import final
 
 ## Definición de la clase Mensaje ##
-class Mensaje:
-    emoji =""
-    text = ""
+class IDP:
+    callingPartyNumber =""
+    mscAddress = ""
+    calledPartyNumber = ""
 ## Definición de la clase Mensaje ##
+
+class Cue:
+    continuar = ""
+
+class RRB:
+    o_answer = bool
+    o_disc = bool
+
+class ERB:
+    o_answer = bool
+    o_disc = bool
+
+class ACH:
+    tiempo = int
+
+class ACR:
+    tiempo = int
 
 s = socket.socket()                                                 #Crear socket
-host = input(str("Please enter the hostname of the server : "))
+host = input(str("Please enter the mscAddress : "))
 port = 8080
 s.connect((host,port))                                              #vincular
 name = input(str("Please enter your username : "))
-print(" Connected to chat server")
+print(" Connected to the central")
 
 s.send(name.encode())                   
 s_name = s.recv(1024)                                               #Recibir el nombre de usario del servidor
 s_name = s_name.decode()
 print("")
 print(s_name, "has joined the chat room ")
+tiempoacr = 0
 
 while 1:
-    mensaje = Mensaje()                                         #definir mensaje de clase Mensaje
+    mensaje = IDP()          
+    mensaje.callingPartyNumber =  "3506013309"              #recibir parámetros
+    mensaje.mscAddress =  "91655340000512"   
+    mensaje.calledPartyNumber =  "3144582356"  
+    message = pickle.dumps(mensaje)   
+    s.send(message)
+    print("IDP enviado")
+    print("")                                     #definir mensaje de clase Mensaje
     data = s.recv(4096)
     data_variable = pickle.loads(data)                          #Des-serializar el flujo de datos
-    print(s_name, ":" ,data_variable.emoji,data_variable.text)  #Imprimir los parámetros emoji y text de la variable data_variable
+    print(s_name, ":" ,data_variable.continuar)  #Imprimir los parámetros emoji y text de la variable data_variable
     print("")
-    mensaje = Mensaje()
-    mensaje.emoji=  input(str("Please enter your emoji: "))     #recibir parámetros
-    mensaje.text =  input(str("Please enter your text "))
-    message = pickle.dumps(mensaje)                             #Serializar jerarquía de objetos
-    s.send(message)
+    
+    data = s.recv(4096)
+    data_variable = pickle.loads(data)                          #Des-serializar el flujo de datos
+    print(s_name, ":" ,data_variable.o_answer,data_variable.o_disc)  #Imprimir los parámetros emoji y text de la variable data_variable
+    print("RRB recibido")
+    print("")
+    time.sleep(random.randint(1,10))
+    erb = ERB()
+    erb.o_answer = True
+    erb.o_disc = False
+    erb = pickle.dumps(erb)  
+    inicio = datetime.now()
+    s.send(erb) 
+    print("ERB enviado (o_answer)", inicio)
     print("")
 
+    data = s.recv(4096)
+    data_variable = pickle.loads(data)
+    time.sleep(data_variable.tiempo)   
+    tiempoacr = data_variable.tiempo                      
+    print("ACR(",tiempoacr,")")
+    print("")
+    data = s.recv(4096)
+    data_variable = pickle.loads(data)
+    time.sleep(data_variable.tiempo)   
+    tiempoacr = tiempoacr + data_variable.tiempo                      
+    print("ACR(",tiempoacr,")")
+    print("")
+    data = s.recv(4096)
+    data_variable = pickle.loads(data)
+    time.sleep(data_variable.tiempo)   
+    tiempoacr = tiempoacr + data_variable.tiempo                      
+    print("ACR(",tiempoacr,")")
+    print("")
+
+    erb = ERB()
+    erb.o_answer = False
+    erb.o_disc = True
+    erb = pickle.dumps(erb)  
+    finalizacion = datetime.now()
+    s.send(erb) 
+    print("ERB enviado (o_disc) ", finalizacion)
+    print("Llamada finalizada")
+    print("Duracion:", finalizacion - inicio)
+    print("")
+
+    exit()
--- server.py
+++ server.py
@@ -1,14 +1,32 @@
 import socket
 import sys
 import pickle
-
+import time
+import random
 
 ## Definición de la clase Mensaje ##
-class Mensaje:
-    emoji =""
-    text = ""
+class IDP:
+    callingPartyNumber =""
+    mscAddress = ""
+    calledPartyNumber = ""
 ## Definición de la clase Mensaje ##
 
+class Cue:
+    continuar = ""
+
+class RRB:
+    o_answer = bool
+    o_disc = bool
+
+class ERB:
+    o_answer = bool
+    o_disc = bool
+
+class ACH:
+    tiempo = int
+
+class ACR:
+    tiempo = int
 
 s = socket.socket()                                                                 #Crear socket
 host = socket. gethostname()                                                        #Método para obtener el hostname
@@ -29,16 +47,71 @@ print(s_name, "has joined the chat room")                                       
 conn.send(name.encode())                                                            #Enviar nombre de usuario del servidor                                      
 
 while 1:
-    mensaje = Mensaje()                                         #definir mensaje de clase Mensaje
-    mensaje.emoji=  input(str("Please enter your emoji: "))     #recibir parámetros
-    mensaje.text =  input(str("Please enter your text "))       
-    message = pickle.dumps(mensaje)                             #Serializar jerarquía de objetos
-    conn.send(message)                                          
-    print("Sent")
-    print("")
+    # mensaje = IDP()                                         #definir mensaje de clase Mensaje
+    # mensaje.callingPartyNumber =  "3506013309"              #recibir parámetros
+    # mensaje.mscAddress =  "91655340000512"   
+    # mensaje.calledPartyNumber =  "3144582356"  
+    # message = pickle.dumps(mensaje)                             #Serializar jerarquía de objetos
+    # conn.send(message)                                          
+    # print("Sent")
+    # print("")
     data = conn.recv(4096)                                      #Recibir mensaje de cliente
     data_variable = pickle.loads(data)                          #Des-serializar el flujo de datos
-    print(s_name, ":" ,data_variable.emoji,data_variable.text)  #Imprimir los parámetros emoji y text de la variable data_variable
+    print(s_name, ":" ,data_variable.callingPartyNumber,data_variable.mscAddress, data_variable.calledPartyNumber)  #Imprimir los parámetros emoji y text de la variable data_variable
     print("")
+    time.sleep(2)
+    cue = Cue()
+    cue.continuar =  "Continuar"
+    cue = pickle.dumps(cue)  
+    conn.send(cue)                       
+    print("Sent")
+    print("")
+    time.sleep(2)
+    rrb = RRB()
+    rrb = pickle.dumps(rrb)  
+    conn.send(rrb) 
+    print("RRB enviado")
+    print("")
+    data = conn.recv(4096)
+    data_variable = pickle.loads(data)                          #Des-serializar el flujo de datos
+    print(s_name, ":" ,data_variable.o_answer)
+    print("ERB recibido (o_answer)")
+    print("")
+    if data_variable.o_answer == True:
+        ach = ACH()
+        periodo = 6
+        ach.tiempo = periodo
+        ach = pickle.dumps(ach)
+        conn.send(ach) 
+        print("ACH( 6 )")
+        print("")
+        time.sleep(periodo) 
+        conn.send(ach) 
+        print("ACH( 6 )")
+        print("")
+        time.sleep(periodo)
+        ach = ACH()
+        ach.tiempo = 3
+        ach = pickle.dumps(ach)
+        conn.send(ach) 
+        print("ACH( 3 )")
+        print("")
+        time.sleep(3)
+
+    else:
+        exit()
+
+    data = conn.recv(4096)
+    data_variable = pickle.loads(data)                          #Des-serializar el flujo de datos
+    print(s_name, ":" ,data_variable.o_disc)
+    print("ERB recibido (o_disc)")
+    print("")
+
+    if data_variable.o_disc == True:
+        print("Llamada finalizada")
+        exit()
+
+
+    exit()
 
     
